a49b6c0c988ca882f07660d72ef025959b1a2134
CarlosEduSL <carloslima.adr122@gmail.com>
2026-03-09 22:48:55 -0300

    feat: reestruturar pastas para src, ajustar rotas e conectar MongoDB Atlas

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.json());
 
 // Importando rotas de pedidos
-const orderRoutes = require('../routes/orderRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 // Usando as rotas de pedidos [cite: 15]
 app.use('/order', orderRoutes);
